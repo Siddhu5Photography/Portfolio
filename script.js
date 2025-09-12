2001a908de48c47ff7b252627d644f7d789e2320
Siddhu5Photography <ssidda29@gmail.com>
2025-09-13 01:15:53 +0930

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ document.getElementById("contact-form").addEventListener("submit", function (e) 
   e.preventDefault();
   alert("Thanks for reaching out! I'll get back to you soon. ✅");
 });
+
+function toggleMenu() {
+  document.querySelector('.nav-links').classList.toggle('active');
+}
